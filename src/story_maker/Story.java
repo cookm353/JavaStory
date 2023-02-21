b63055580ed70df1a2483596295c3ba0055c5a49
Matthew Cook <cookm353@gmail.com>
2023-02-21 10:13:52 -0500

    Fixed variable type

--- src/story_maker/Story.java
+++ src/story_maker/Story.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 public class Story {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String adjective, season, cupsTea;
+        String adjective, season;
+        int cupsTea;
 
         System.out.println("Pick an adjective");
         adjective = scanner.next();
         System.out.println("Pick a season");
         season = scanner.next();
         System.out.println("Pick a number");
-        cupsTea = scanner.next();
+        cupsTea = scanner.nextInt();
+
         scanner.close();
 
         String story = String.format("On a %s %s day, I drink a minimum of %s cups of tea.", adjective, season, cupsTea);
