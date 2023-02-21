728784663ce8da2dee45df2791fef700d24e00c3
Matthew Cook <cookm353@gmail.com>
2023-02-21 10:05:42 -0500

    Made repo

---
+++ src/story_maker/Story.java
@@ -0,0 +1,21 @@
+package story_maker;
+
+import java.util.Scanner;
+
+public class Story {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String adjective, season, cupsTea;
+
+        System.out.println("Pick an adjective");
+        adjective = scanner.next();
+        System.out.println("Pick a season");
+        season = scanner.next();
+        System.out.println("Pick a number");
+        cupsTea = scanner.next();
+        scanner.close();
+
+        String story = String.format("On a %s %s day, I drink a minimum of %s cups of tea.", adjective, season, cupsTea);
+        System.out.println(story);
+    }
+}
